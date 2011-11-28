d94d671e7f18568a6d580563ab642afdc2f3ee8c
Steve Kondik <shade@chemlab.org>
2011-11-27 22:10:54 -0800

    Set tags to debug and remove use of ALL_PREBUILT.
    Change-Id: Ia7a6c58dd213862f8dce0fea5ca7d2d6ac3867da

--- Android.mk
+++ Android.mk
@@ -36,7 +36,7 @@ LOCAL_STATIC_LIBRARIES += \
 	libclearsilverregex
 LOCAL_MODULE := nano
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := debug
 include $(BUILD_EXECUTABLE)
 
 
@@ -45,14 +45,18 @@ include $(BUILD_EXECUTABLE)
 # ========================================================
 etc_files := $(shell ls -1 $(LOCAL_PATH)/etc/)
 
-copy_to := $(addprefix $(TARGET_OUT)/etc/$(LOCAL_MODULE)/,$(etc_files))
-copy_from := $(addprefix $(LOCAL_PATH)/etc/,$(etc_files))
+NANO_ETC := $(TARGET_OUT)/etc/$(LOCAL_MODULE)/
+NANO_CONFIGS := $(addprefix $(LOCAL_PATH)/etc/,$(etc_files))
+$(NANO_CONFIGS): NANO_BINARY := $(LOCAL_MODULE)
+$(NANO_CONFIGS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Install: $@ -> $(NANO_ETC)"
+	@mkdir -p $(NANO_ETC)
+	$(hide) cp $@ $(NANO_ETC)
 
-$(copy_to) : PRIVATE_MODULE := system_etcdir
-$(copy_to) : $(TARGET_OUT)/etc/$(LOCAL_MODULE)/% : $(LOCAL_PATH)/etc/% | $(ACP)
-	$(transform-prebuilt-to-target)
+ALL_DEFAULT_INSTALLED_MODULES += $(NANO_CONFIGS)
 
-ALL_PREBUILT += $(copy_to)
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+	$(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(NANO_CONFIGS)
 
 # ========================================================
 include $(call all-makefiles-under,$(LOCAL_PATH))
