be86c9f37df192deebe5c09f287447d47881b78c
Kevin F. Haggerty <haggertk@lineageos.org>
2019-07-08 20:46:26 -0600

    nano: Zero entire struct sigaction struct
    Fixes the below on some devices:
    external/nano/src/nano.c:863:30: error: missing field 'sa_mask' initializer [-Werror,-Wmissing-field-initializers]
            struct sigaction deed = {{0}};
                                        ^
    external/nano/src/files.c:948:46: error: missing field 'sa_mask' initializer [-Werror,-Wmissing-field-initializers]
            struct sigaction oldaction, newaction = {{0}};
                                                        ^
    
    Change-Id: I7f806e94e7ca4ec46d1bfc91569ecdd15188aa8f

--- src/files.c
+++ src/files.c
@@ -945,7 +945,8 @@ bool execute_command(const char *command)
 		/* The pipes through which text will be written and read. */
 	const bool should_pipe = (command[0] == '|');
 	FILE *stream;
-	struct sigaction oldaction, newaction = {{0}};
+	struct sigaction oldaction, newaction;
+	memset(&oldaction, 0, sizeof(struct sigaction));
 		/* Original and temporary handlers for SIGINT. */
 
 	/* Create a pipe to read the command's output from, and, if needed,
